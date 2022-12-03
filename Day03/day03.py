79253336dcf9d454ba7fc4746b5810fae6d6369d
Fredrik Wallin <fredrik.wallin.91@gmail.com>
2022-12-03 10:08:35 +0100

    Solution Advent of code day 3 part 2

--- Day03/day03.py
+++ Day03/day03.py
@@ -1,7 +1,18 @@
 
 filePath = 'Day03/inventoryContent.txt'
 
-# Wrong: 9830
+def calcPrio(mychar):
+    val = ord(mychar)
+    # Check if char in lower case or upper case.
+    # If ASCII code is larger than 96, its lower.
+    if val > 96:
+        # 'a' shall result in 1, hence the offset is -96
+        # from ASCII code valie
+        valOffset = -96
+    else:
+        # For upper case, the offsert is smaller
+        valOffset = -64+26
+    return val + valOffset
 
 with open(filePath) as f:
     mylist = f.read().splitlines()
@@ -12,20 +23,31 @@ with open(filePath) as f:
         str1 = row[:int(len(row)/2)]
         str2 = row[int(len(row)/2):]
 
-        for mychar in str1:
-            numOfChar = str2.count(mychar)
+        for myChar in str1:
+            numOfChar = str2.count(myChar)
             if numOfChar > 0:
-                val = ord(mychar)
-                # Check if char in lower case or upper case.
-                # If ASCII code is larger than 96, its lower.
-                if val > 96:
-                    # 'a' shall result in 1, hence the offset is -96
-                    # from ASCII code valie
-                    valOffset = -96
-                else:
-                    # For upper case, the offsert is smaller
-                    valOffset = -64+26
-                sum += val + valOffset
+                prio = calcPrio(myChar)
+                sum += prio
                 break
 
-print(f"Sum of item: {sum}")
+print(f"Sum of item (part 1): {sum}")
+
+with open(filePath) as f:
+    mylist = f.read().splitlines()
+    sum2 = 0
+
+    numLines = len(mylist)
+    i = 0
+    while i<numLines:
+        s1 = set(mylist[i])
+        i += 1
+        s2 = set(mylist[i])
+        i += 1
+        s3 = set(mylist[i])
+        i += 1
+
+        myChar = s1.intersection(s2).intersection(s3).pop()
+        sum2 += calcPrio(myChar)
+        
+print(f"Sum of item (part 2): {sum2}")
+# Wrong: 1300
