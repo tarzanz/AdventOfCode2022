7dbb7892d66f8249fdf957be0fb60a2f347c416c
Fredrik Wallin <fredrik.wallin.91@gmail.com>
2022-12-04 10:48:11 +0100

    Solution Advent of code day 04 part 2

--- Day04/day04.py
+++ Day04/day04.py
@@ -8,7 +8,6 @@ def StrToNums(myStr):
 def OneContainTheOther(str1,str2):
     n1Low, n1High = StrToNums(str1)
     n2Low, n2High = StrToNums(str2)
-
     
     if n1Low <= n2Low and n1High >= n2High:
     # If n1 contains n2
@@ -22,12 +21,30 @@ def OneContainTheOther(str1,str2):
     # One does not completely overlap the other
         return False
 
+def Overlapping(str1,str2):
+    n1Low, n1High = StrToNums(str1)
+    n2Low, n2High = StrToNums(str2)
+
+    if n2Low <= n1Low and n2High >= n1Low:
+        return True
+    elif n1Low <= n2Low and n1High >= n2Low:
+        return True
+    else:
+        # The segments does not overlap at all
+        return False
+
+
 with open(filePath) as f:
     mylist = f.read().splitlines()
-    counter = 0
+    counterPart1 = 0
+    counterPart2 = 0
 
     for row in mylist:
         splitLine = row.split(',')
         if OneContainTheOther(splitLine[0],splitLine[1]):
-            counter += 1
-print(f"Number of overlaping assignments: {counter}")
+            counterPart1 += 1
+        if Overlapping(splitLine[0],splitLine[1]):
+            counterPart2 += 1
+
+print(f"Number of completely overlaping assignments (part 1): {counterPart1}")
+print(f"Number of partly overlaping assignments (part 2): {counterPart2}")
